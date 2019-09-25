e7d2db1eae56ba05ec825ac6a0f27e6e17a945ba
TheAsianCow <jeffreyhuangdragon@gmail.com>
2019-09-25 15:53:37 -0400

    Add files via upload

---
+++ Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430f5529.h
+main.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics_legacy_undefs.h
+main.obj: ../peripherals.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_types.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_stdint.h
+main.obj: C:/Users/Jeffrey\ Huang/Documents/WPI/2019-2020/A\ term/ECE2049/Labs/Lab3TimeTempDisp/grlib/grlib.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdbool.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/assert.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/_ti_config.h
+main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/linkage.h
+main.obj: ../LcdDriver/Sharp96x96.h
+main.obj: ../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h
+
+../main.c: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics_legacy_undefs.h: 
+../peripherals.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_stdint.h: 
+C:/Users/Jeffrey\ Huang/Documents/WPI/2019-2020/A\ term/ECE2049/Labs/Lab3TimeTempDisp/grlib/grlib.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/assert.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/linkage.h: 
+../LcdDriver/Sharp96x96.h: 
+../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h: 
